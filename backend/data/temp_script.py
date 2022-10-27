ca7dd50e51b4f13de3d4453c817f8b896ec3c194
Vignesh Rao <vigneshravirao@gmail.com>
2022-10-27 23:23:02 +0530

    Fix divisions.csv

--- backend/data/temp_script.py
+++ backend/data/temp_script.py
@@ -4,9 +4,9 @@ import os
 
 # tables = camelot.read_pdf(
 #     "freshers.pdf", pages='1-end', strip_text='\n')
-# table_dfs = [table.df.iloc[1:, :] for table in tables]
+# table_dfs = [table.df.iloc[:, :] for table in tables]
 # final_df = pd.concat(table_dfs, ignore_index=True)
-# final_df.to_csv('fresher_groups.csv', index=False)
+# final_df.to_csv('fresher_groups_new.csv', index=False)
 
 # df = pd.read_csv("fresher_groups.csv")
 # df.columns = df.iloc[0]
