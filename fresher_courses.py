57742d2ae0ec5088c6eb9b5f0b9f238cca41247b
Hareesh-Nandigrama <85237630+Hareesh-Nandigrama@users.noreply.github.com>
2023-07-25 17:16:20 +0000

    temp changes for freshers

--- fresher_courses.py
+++ fresher_courses.py
@@ -9,7 +9,7 @@ import helper
 
 
 def get_designfresher_courses(roll_number):
-    data_map=ocr.get_fresher_DF(roll_number)
+    #data_map=ocr.get_fresher_DF(roll_number)
 
     # Correct for div 3
     courses=[
@@ -32,18 +32,21 @@ def get_designfresher_courses(roll_number):
             'endsem':' '
         },
     ]
-    if data_map['Division']=='III' or data_map['Division']=='IV':
-        for c in courses:
-            c['slot']=c['slot']+'1'
-    if data_map['Division']=='II' or data_map['Division']=='IV':
-        for c in courses:
-            c['instructor']='5G2'
+    # if data_map['Division']=='III' or data_map['Division']=='IV':
+    #     for c in courses:
+    #         c['slot']=c['slot']+'1'
+    # if data_map['Division']=='II' or data_map['Division']=='IV':
+    #     for c in courses:
+    #         c['instructor']='5G2'
+    for c in courses:
+        c['instructor'] = ''
     tutorial=[
         {
             'course':'MA 101 Tutorial',
             'code':'MA101',
             'slot':'b',
-            'instructor':data_map['Location'],
+            # 'instructor':data_map['Location'],
+            'instructor':'',
             'ltpc':' ',
             'midsem':' ',
             'endsem':' '
@@ -53,7 +56,8 @@ def get_designfresher_courses(roll_number):
         {
             'course':'CE 110 Lab',
             'code':'CE110',
-            'slot':'AL' if data_map['Division'] in ['III','IV'] else 'ML',
+            # 'slot':'AL' if data_map['Division'] in ['III','IV'] else 'ML',
+            'slot':'AL',
             'instructor':'Engineering Drawing (Practical): 1203 and 1204, Academic Complex (AC)',
             'ltpc':' ',
             'midsem':' ',
@@ -71,7 +75,7 @@ def get_designfresher_courses(roll_number):
     }
 
 def get_fresher_courses(roll_number):
-    data_map=ocr.get_fresher_DF(roll_number)
+    # data_map=ocr.get_fresher_DF(roll_number)
 
     # Correct for div 3
     courses=[
@@ -121,18 +125,21 @@ def get_fresher_courses(roll_number):
             'endsem':' '
         }
     ]
-    if data_map['Division']=='I' or data_map['Division']=='II':
-        for c in courses:
-            c['slot']=c['slot']+'1'
-    if data_map['Division']=='II' or data_map['Division']=='IV':
-        for c in courses:
-            c['instructor']='5G2'
+    # if data_map['Division']=='I' or data_map['Division']=='II':
+    #     for c in courses:
+    #         c['slot']=c['slot']+'1'
+    # if data_map['Division']=='II' or data_map['Division']=='IV':
+    #     for c in courses:
+    #         c['instructor']='5G2'
+    for c in courses:
+        c['instructor'] = ''
     tutorial=[
         {
             'course':'MA 101 Tutorial',
             'code':'MA101',
             'slot':'b',
-            'instructor':data_map['Location'],
+            # 'instructor':data_map['Location'],
+            'instructor': '',
             'ltpc':' ',
             'midsem':' ',
             'endsem':' '
@@ -141,7 +148,8 @@ def get_fresher_courses(roll_number):
             'course':'EE 101 Tutorial',
             'code':'EE101',
             'slot':'c',
-            'instructor':data_map['Location'],
+            'instructor': '',
+            # 'instructor':data_map['Location'],
             'ltpc':' ',
             'midsem':' ',
             'endsem':' '
@@ -149,8 +157,9 @@ def get_fresher_courses(roll_number):
         {
             'course':'CH 101 Tutorial',
             'code':'CH101',
+            'instructor': '',
             'slot':'d',
-            'instructor':data_map['Location'],
+            # 'instructor':data_map['Location'],
             'ltpc':' ',
             'midsem':' ',
             'endsem':' '
@@ -159,7 +168,8 @@ def get_fresher_courses(roll_number):
             'course':'PH 101 Tutorial',
             'code':'PH101',
             'slot':'e',
-            'instructor':data_map['Location'],
+            'instructor': '',
+            # 'instructor':data_map['Location'],
             'ltpc':' ',
             'midsem':' ',
             'endsem':' '
@@ -169,17 +179,19 @@ def get_fresher_courses(roll_number):
         {
             'course':'CH 110 Lab',
             'code':'CH110',
-            'slot':'ML' if data_map['Division'] in ['II','I'] else 'AL',
+            # 'slot':'ML' if data_map['Division'] in ['II','I'] else 'AL',
+            'slot': "AL",
             'instructor':'Chemistry Laboratory: Department of Chemistry, Academic Complex (AC) ',
             'ltpc':' ',
             'midsem':' ',
             'endsem':' '
         },
         {
-            'course':'PH 110 Lab' if data_map['Division'] in ['II','I'] else 'ME 110 Lab',
-            'code':'PH110' if data_map['Division'] in ['II','I'] else 'ME110',
-            'slot':'AL' if data_map['Division'] in ['III','IV'] else 'ML',
-            'instructor':'Department of Physics, Academic Complex (AC)' if data_map['Division'] in ['II','I'] else 'Workshop (on the western side of Academic Complex (AC))',
+            # 'course':'PH 110 Lab' if data_map['Division'] in ['II','I'] else 'ME 110 Lab',
+            # 'code':'PH110' if data_map['Division'] in ['II','I'] else 'ME110',
+            'slot': "AL",
+            # 'slot':'AL' if data_map['Division'] in ['III','IV'] else 'ML',
+            # 'instructor':'Department of Physics, Academic Complex (AC)' if data_map['Division'] in ['II','I'] else 'Workshop (on the western side of Academic Complex (AC))',
             'ltpc':' ',
             'midsem':' ',
             'endsem':' '
@@ -187,33 +199,34 @@ def get_fresher_courses(roll_number):
         {
             'course':'CE 110 Lab',
             'code':'CE110',
-            'slot':'AL' if data_map['Division'] in ['III','IV'] else 'ML',
+            'slot': "AL",
+            # 'slot':'AL' if data_map['Division'] in ['III','IV'] else 'ML',
             'instructor':'Engineering Drawing (Practical): 1203 and 1204, Academic Complex (AC)',
             'ltpc':' ',
             'midsem':' ',
             'endsem':' '
         },
     ]
-    if data_map['Lab']=='L6' or data_map['Lab']=='L1':
-        lab[0]['slot']=lab[0]['slot']+'1'
-        lab[1]['slot']=lab[1]['slot']+'4'
-        lab[2]['slot']=lab[2]['slot']+'2'
-    elif data_map['Lab']=='L7' or data_map['Lab']=='L2':
-        lab[0]['slot']=lab[0]['slot']+'3'
-        lab[1]['slot']=lab[1]['slot']+'1'
-        lab[2]['slot']=lab[2]['slot']+'4'
-    elif data_map['Lab']=='L8' or data_map['Lab']=='L3':
-        lab[0]['slot']=lab[0]['slot']+'5'
-        lab[1]['slot']=lab[1]['slot']+'3'
-        lab[2]['slot']=lab[2]['slot']+'1'
-    elif data_map['Lab']=='L9' or data_map['Lab']=='L4':
-        lab[0]['slot']=lab[0]['slot']+'2'
-        lab[1]['slot']=lab[1]['slot']+'5'
-        lab[2]['slot']=lab[2]['slot']+'3'
-    elif data_map['Lab']=='L10' or data_map['Lab']=='L5':
-        lab[0]['slot']=lab[0]['slot']+'4'
-        lab[1]['slot']=lab[1]['slot']+'2'
-        lab[2]['slot']=lab[2]['slot']+'5'
+    # if data_map['Lab']=='L6' or data_map['Lab']=='L1':
+    #     lab[0]['slot']=lab[0]['slot']+'1'
+    #     lab[1]['slot']=lab[1]['slot']+'4'
+    #     lab[2]['slot']=lab[2]['slot']+'2'
+    # elif data_map['Lab']=='L7' or data_map['Lab']=='L2':
+    #     lab[0]['slot']=lab[0]['slot']+'3'
+    #     lab[1]['slot']=lab[1]['slot']+'1'
+    #     lab[2]['slot']=lab[2]['slot']+'4'
+    # elif data_map['Lab']=='L8' or data_map['Lab']=='L3':
+    #     lab[0]['slot']=lab[0]['slot']+'5'
+    #     lab[1]['slot']=lab[1]['slot']+'3'
+    #     lab[2]['slot']=lab[2]['slot']+'1'
+    # elif data_map['Lab']=='L9' or data_map['Lab']=='L4':
+    #     lab[0]['slot']=lab[0]['slot']+'2'
+    #     lab[1]['slot']=lab[1]['slot']+'5'
+    #     lab[2]['slot']=lab[2]['slot']+'3'
+    # elif data_map['Lab']=='L10' or data_map['Lab']=='L5':
+    #     lab[0]['slot']=lab[0]['slot']+'4'
+    #     lab[1]['slot']=lab[1]['slot']+'2'
+    #     lab[2]['slot']=lab[2]['slot']+'5'
     
     for i in range(len(courses)):
         courses[i]['midsem'] = helper.get_midsem_time(courses[i]['slot'])
