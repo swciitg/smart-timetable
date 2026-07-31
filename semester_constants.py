e7f033ea7ceea5d9cfe4f9071fd0ed2ae911db13
ganeshsinnur <ganeshsinnur07@gmail.com>
2026-07-31 19:58:59 +0530

    refactor: update course schedule data

--- semester_constants.py
+++ semester_constants.py
@@ -1,6 +1,6 @@
-FRESHER_YEAR = '25'        # Starting 2 digits of this users freshers roll number
-# SEM_SESSION = 'July-Nov'    # Session of semester 
-SEM_SESSION = 'Jan-May'
+FRESHER_YEAR = '26'        # Starting 2 digits of this users freshers roll number
+SEM_SESSION = 'July-Nov'    # Session of semester 
+# SEM_SESSION = 'Jan-May'
 SEM_YEAR = '2026'          # Year of the session
 
 # Mid sem and End sem timings according to slots
